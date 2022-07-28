fea01d2bc1c92a8018faee0238845cfca3f9f2ed
Mohammad Emad <memad.20003@gmail.com>
2022-07-28 11:55:41 +0200

    first commit

---
+++ Mohammad Amer/images/lib/main.dart
@@ -0,0 +1,38 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/Jobs.jpg'),
+              ),
+              Text('Steve Jobs'),
+            ],
+          ),
+        ),
+
+        ),
+      );
+  }
+}
